5a7edaabe2421e5883ae45605096d42996cd37e4
yazilimkulubutr <yazilimkulubu@hotmail.com>
2015-12-04 22:12:20 +0200

    Start

---
+++ metodlaryazilimkulubu/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodlaryazilimkulubu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Toplama(3, 5);
+            Toplama(4, 5);
+            Toplama(9, 3);
+            Toplama(4, 5);
+            Console.ReadLine();
+        }
+
+        private static void Toplama(int sayi1, int sayi2)
+        {
+            Console.WriteLine(sayi1 + sayi2);
+
+        }
+    }
+}
